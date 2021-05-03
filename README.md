7aba9f2b4135a49a87924d2fdf35becd5f68054f
workingNight <43333010+workingNight@users.noreply.github.com>
2021-05-03 22:59:55 +0800

    万里长城永不倒

--- README.md
+++ README.md
@@ -7,5 +7,6 @@
 2. react + react hooks 
 3. node 相关复习一下
 4. webpack的相关复习一下
+5. 学习下ant-design的源码，能够学习大厂的优秀开发习惯，多深入了解一些组件相关和less
 
 
--- demo2/src/App.js
+++ demo2/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
 
+function Welcome(props) {
+  return <h2>hello , {props.name}</h2>
+}
+
 function App() {
   return (
-    <h1>hello , react</h1>
+    <div>
+      <h1>hello , react</h1>
+
+
+      <Welcome name="bob"></Welcome>
+      <Welcome name="jack"></Welcome>
+      <Welcome name="pdd"></Welcome>
+    </div>
   )
 }
 
